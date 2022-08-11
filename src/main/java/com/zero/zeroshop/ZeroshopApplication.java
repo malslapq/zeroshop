fdd7732029774366f70d25c197dd46f10ac80e22
SeongHyeon <malslapq@gmail.com>
2022-08-12 01:08:30 +0900

    Initial commit

---
+++ src/main/java/com/zero/zeroshop/ZeroshopApplication.java
@@ -0,0 +1,13 @@
+package com.zero.zeroshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zero/zeroshop/ZeroshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.zeroshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
