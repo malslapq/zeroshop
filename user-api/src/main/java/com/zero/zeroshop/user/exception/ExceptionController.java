0a3d3ce209e983edcb6feac22721dff25e569b22
SeongHyeon <malslapq@gmail.com>
2022-08-15 21:03:52 +0900

    feat : [003-EMAIL-VERIFICATION] 커스터머(고객) 이메일 verification

---
+++ user-api/src/main/java/com/zero/zeroshop/user/exception/ExceptionController.java
@@ -0,0 +1,28 @@
+package com.zero.zeroshop.user.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionController {
+
+    @ExceptionHandler({CustomerException.class})
+    public ResponseEntity<ExceptionResponse> customRequestException(final CustomerException e) {
+        log.warn("api Exception : {}", e.getErrorCode());
+        return ResponseEntity.badRequest().body(new ExceptionResponse(e.getMessage(), e.getErrorCode()));
+    }
+
+    @Getter
+    @ToString
+    @AllArgsConstructor
+    public static class ExceptionResponse {
+        private String message;
+        private ErrorCode errorCode;
+    }
+}
--- user-api/src/main/java/com/zero/zeroshop/user/service/SignUpCustomerService.java
+++ user-api/src/main/java/com/zero/zeroshop/user/service/SignUpCustomerService.java
@@ -3,8 +3,14 @@ package com.zero.zeroshop.user.service;
 import com.zero.zeroshop.user.domain.SignUpForm;
 import com.zero.zeroshop.user.domain.model.Customer;
 import com.zero.zeroshop.user.domain.repository.CustomerRepository;
+import com.zero.zeroshop.user.exception.CustomerException;
+import com.zero.zeroshop.user.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +20,36 @@ public class SignUpCustomerService {
 
     public Customer signUp(SignUpForm form) {
         return customerRepository.save(Customer.from(form));
+    }
+
+    public boolean isEmailExist(String email) {
+        return customerRepository.findByEmail(email.toLowerCase(Locale.ROOT)).isPresent();
+    }
+
+    @Transactional
+    public void verifyEmail(String email, String code) {
+        Customer customer =
+                customerRepository.findByEmail(email).orElseThrow(
+                        () -> new CustomerException(ErrorCode.NOT_FOUND_USER_FROM_EMAIL));
+        if (customer.isVerify()) {
+            throw new CustomerException(ErrorCode.ALREADY_VERIFY);
+        }
+        if (!customer.getVerificationCode().equals(code)) {
+            throw new CustomerException(ErrorCode.WRONG_VERIFICATION);
+        }
+        if (customer.getVerifyExpiredAt().isBefore(LocalDateTime.now())) {
+            throw new CustomerException(ErrorCode.EXPIRE_CODE);
+        }
+        customer.verificationCompleted();
+    }
+
+    @Transactional
+    public LocalDateTime ChangeCustomerValidateEmail(Long CustomerId, String verificationCode) {
+        Customer customer =
+                customerRepository.findById(CustomerId).orElseThrow(
+                        () -> new CustomerException(ErrorCode.NOT_FOUND_USER_FROM_EMAIL));
+        customer.changeVerifications(verificationCode);
+        return customer.getVerifyExpiredAt();
     }
 
 }
