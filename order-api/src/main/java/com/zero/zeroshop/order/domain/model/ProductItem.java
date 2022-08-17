7e680ada314fa6a4afeaf579457b358f0402eeea
SeongHyeon <malslapq@gmail.com>
2022-08-17 21:26:02 +0900

    feat : [011-Product_DELETE] Product & ProductItem Delete

--- order-api/src/main/java/com/zero/zeroshop/order/domain/model/ProductItem.java
+++ order-api/src/main/java/com/zero/zeroshop/order/domain/model/ProductItem.java
@@ -29,7 +29,7 @@ public class ProductItem extends BaseEntity{
     private Integer price;
 
     private Integer count;
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
 
